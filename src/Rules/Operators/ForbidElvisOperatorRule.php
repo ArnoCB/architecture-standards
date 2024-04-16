9b46f3a2cdcb2064f160010bd8a08552a6439a1d
ArnoCB <a.bouwes@upcmail.nl>
2024-04-16 20:55:33 +0200

    refactor elvis rule test

--- src/Rules/Operators/ForbidElvisOperatorRule.php
+++ src/Rules/Operators/ForbidElvisOperatorRule.php
@@ -4,7 +4,6 @@ namespace Rules\Operators;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Ternary;
-use PhpParser\PrettyPrinter\Standard;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleError;
@@ -29,14 +28,10 @@ class ForbidElvisOperatorRule implements Rule
      */
     public function processNode(Node $node, Scope $scope): array
     {
-        $errors = [];
-
-        if ($node->if === null) {
-            $errors[] = RuleErrorBuilder::message(
-                'Usage of the Elvis operator is forbidden.'
-            )->build();
+        if ($node->if !== null) {
+            return [];
         }
 
-        return $errors;
+        return [RuleErrorBuilder::message('Usage of the Elvis operator is forbidden.')->build()];
     }
 }
